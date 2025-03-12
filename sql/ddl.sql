53448cd7666ed987f516f483a58ddb8b9565e0e8
viniciusrabello <viniciusrabello@matematica.ufrj.br>
2025-03-11 21:52:25 -0300

    added db connection and ddl for users table

---
+++ sql/ddl.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    email VARCHAR(255) NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (email)
+);
