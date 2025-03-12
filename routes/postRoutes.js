391d18bad17eb43d1930d2715b22066c4cdb6bfc
viniciusrabello <viniciusrabello@matematica.ufrj.br>
2025-03-12 13:45:35 -0300

    removed post controllers and routes

--- routes/postRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const postController = require('../controllers/postController');
-const authenticateToken = require('../middleware/authenticate');
-
-router.get('/', authenticateToken, postController.getPosts);
-
-module.exports = router;
