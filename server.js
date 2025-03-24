dfd0c48a1bcdd68ff53eddfa775c2f0f0bbdb16c
viniciusrabello <viniciusrabello@matematica.ufrj.br>
2025-03-24 01:33:51 -0300

    created summary and calendar views, still empty for now

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const userRoutes = require('./routes/userRoutes');
 const homeRoutes = require('./routes/homeRoutes');
 const subjectRoutes = require('./routes/subjectRoutes');
 const eventRoutes = require('./routes/eventRoutes');
+const calendarRoutes = require('./routes/calendarRoutes');
+const summaryRoutes = require('./routes/summaryRoutes');
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'views')));
@@ -17,6 +19,8 @@ app.use('/users', userRoutes);
 app.use('/home', homeRoutes);
 app.use('/subjects', subjectRoutes);
 app.use('/events', eventRoutes);
+app.use('/calendar', calendarRoutes);
+app.use('/summary', summaryRoutes);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/index/index.html'));
